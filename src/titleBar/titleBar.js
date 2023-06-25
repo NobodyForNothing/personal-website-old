ed24b1a00dc45b51ad874bf3a27679f46f82780d
NobodyForNothing <82763757+NobodyForNothing@users.noreply.github.com>
2023-06-25 12:23:48 +0200

    add page navigation

--- src/titleBar/titleBar.js
+++ src/titleBar/titleBar.js
@@ -9,6 +9,7 @@ export function titleBar() {
     barBg.id = 'title-bar-bg';
     
     barBg.appendChild(derdillaComTitle());
+    barBg.appendChild(sectionsBar());
     bar.appendChild(barBg)
     
     return bar;
@@ -19,4 +20,20 @@ function derdillaComTitle() {
     title.innerText = 'DERDILLA.COM';
     title.id = 'derdilla-com-title';
     return title;
+}
+
+const sections = ['about', 'projects', 'faq'];
+function sectionsBar() {
+    const sectContain = document.createElement('div');
+    sectContain.id = 'sections-bar';
+
+    for (const sectName of sections) {
+        const sectBtn = document.createElement('a');
+        sectBtn.classList.add('section-btn');
+        sectBtn.innerText = sectName;
+        sectBtn.href = `#${sectName}`;
+        sectContain.appendChild(sectBtn);
+    }
+
+    return sectContain;
 }
