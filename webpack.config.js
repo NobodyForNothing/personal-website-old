83caddbb22674952b1d45b3460f2c0e7f1de38c7
NobodyForNothing <82763757+NobodyForNothing@users.noreply.github.com>
2023-06-28 15:36:19 +0200

    use font from google fonts

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,32 @@
 const path = require('path');
 
-module.exports = {
- entry: './src/index.js',
- output: {
-  filename: 'main.js',
-  path: path.resolve(__dirname, 'dist'),
- },
- mode: 'development',
- module: {
-  rules: [
-   {
-    test: /\.(scss|css)$/,
-    use: ['style-loader', 'css-loader', 'sass-loader'],
-   },
-   {
-    test: /\.(png|svg|jpg|jpeg|gif)$/i,
-    type: 'asset/resource',
-   },
-   {
-    test: /\.(woff|woff2|eot|ttf|otf)$/i,
-    type: 'asset/resource',
-   },
-  ],
- },
- devServer: {
-  static: './dist',
- },
-}; 
+module.exports = env => {
+    console.log(env);
+    return {
+        entry: './src/index.js',
+        output: {
+         filename: 'main.js',
+         path: path.resolve(__dirname, 'dist'),
+        },
+        mode: env.developement ? 'development' : 'production',
+        module: {
+         rules: [
+          {
+           test: /\.(scss|css)$/,
+           use: ['style-loader', 'css-loader', 'sass-loader'],
+          },
+          {
+           test: /\.(png|svg|jpg|jpeg|gif)$/i,
+           type: 'asset/resource',
+          },
+          {
+           test: /\.(woff|woff2|eot|ttf|otf)$/i,
+           type: 'asset/resource',
+          },
+         ],
+        },
+        devServer: {
+         static: './dist',
+        },
+       };
+}
